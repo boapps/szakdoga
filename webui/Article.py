4bede0069ade2517f5d9488f4b0dc4a39aecb675
boa <boa@protonmail.com>
2023-09-14 23:15:14 +0200

    refactor constructor of Artcile

--- webui/Article.py
+++ webui/Article.py
@@ -56,29 +56,43 @@ class Article:
     def __init__(self, url: str):
         self.url: str = clear_url(url)
 
-        news_article = newspaper.Article(url)
-        news_article.download()
-        news_article.parse()
+        self.news_article = newspaper.Article(self.url)
+        self.download()
+        self.parse()
+        
+        self.prepare_title()
+        self.prepare_text()
+        self.prepare_description()
+        self.prepare_keywords()
+        self.prepare_date()
 
-        self.title: str = news_article.title
-        self.description: str = news_article.meta_description
-        self.text: str = news_article.text+'\n'
-        self.keywords: [str] = news_article.keywords
-        if news_article.publish_date is not None:
-            self.date: str = news_article.publish_date.strftime('%Y. %m. %d. %H:%M:%S')
-        else:
-            self.date: str = ''
+        self.text = do_replacements(self.text, replacements).strip()
+        self.description = do_replacements(self.description, replacements).strip()
+        self.title = do_replacements(self.title, replacements).strip()
 
-        self.text = do_replacements(self.text, replacements)
-        self.description = do_replacements(self.description, replacements)
+    def download(self):
+        self.news_article.download()
+
+    def parse(self):
+        self.news_article.parse()
+
+    def prepare_title(self):
+        self.title: str = self.news_article.title
         self.title = do_replacements(self.title, replacements)
 
-        for description in common_descriptions:
-            self.description = self.description.replace(description.strip(), '')
-
+    def prepare_text(self):
+        self.text: str = self.news_article.text+'\n'
+        self.text = do_replacements(self.text, replacements)
         for line in common_lines:
             if line in self.text:
                 self.text = self.text.replace('\n'+line, '\n')
+
+    def prepare_description(self):
+        self.description = self.news_article.meta_description
+        self.description = do_replacements(self.description, replacements)
+
+        for description in common_descriptions:
+            self.description = self.description.replace(description.strip(), '')
 
         if len(self.description) < 1 and self.text.count('\n') > 1:
             sl = self.text.splitlines()[0]
@@ -86,10 +100,16 @@ class Article:
             if '.' not in sl[:400]:
                 self.description = sl[:400]
 
-        self.text = do_replacements(self.text, replacements).strip()
-        self.description = do_replacements(self.description, replacements).strip()
-        self.title = do_replacements(self.title, replacements).strip()
-        
+    def prepare_keywords(self):
+        self.keywords = self.news_article.keywords
+
+    def prepare_date(self):
+        self.date = self.news_article.publish_date
+        if self.date is not None:
+            self.date = self.date.strftime('%Y. %m. %d. %H:%M:%S')
+        else:
+            self.date = ''
+
     def __str__(self):
         return json.dumps(dict(self), ensure_ascii=False)
 
