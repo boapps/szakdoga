2fec8b113ca16f8c7369fef0ddffcbd033df0e65
boa <boa@protonmail.com>
2023-10-04 23:49:19 +0200

    add pagination

--- webapp/auto_kmdb/routes.py
+++ webapp/auto_kmdb/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 
 from auto_kmdb.Article import Article
 
@@ -7,4 +7,6 @@ api = Blueprint('api', __name__, url_prefix='/api')
 
 @api.route('/articles', methods=["GET"])
 def api_articles():
-    return jsonify([a.dict() for a in Article.query.filter_by(is_classified=True).all()]), 200
+    page = request.args.get('page', 1, type=int)
+    pagination = Article.query.filter_by(is_corruption=True).order_by(Article.date).paginate(page=page, per_page=10)
+    return jsonify({'pages': pagination.pages, 'articles': [a.dict() for a in pagination]}), 200
