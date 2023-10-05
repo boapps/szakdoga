164818d17b6fc90fe4c260647713e5b2fdba1249
boa <boa@protonmail.com>
2023-10-05 16:37:31 +0200

    extend articles' json representation

--- webapp/auto_kmdb/Article.py
+++ webapp/auto_kmdb/Article.py
@@ -81,11 +81,11 @@ class Article(db.Model):
     description = db.Column(db.String)
     date = db.Column(db.String)
     keywords = db.relationship('Tag', secondary=article_tag, backref=db.backref('articles', lazy='dynamic'))
-    
+
     is_classified = db.Column(db.Boolean, default=False)
     is_classified_corruption = db.Column(db.Boolean, default=False)
     classification_score = db.Column(db.Integer, default=0)
-    
+
     is_annoted = db.Column(db.Boolean, default=False)
     is_annoted_corruption = db.Column(db.Boolean, default=True)
 
@@ -156,4 +156,9 @@ class Article(db.Model):
         return self.__str__()
 
     def dict(self):
-        return {'url': self.url, 'title': self.title, 'text': self.text, 'description': self.description, 'keywords': self.keywords, 'date': self.date}
+        return {
+            'id': self.id, 'classification_score': self.classification_score,
+            'url': self.url, 'title': self.title, 'text': self.text,
+            'description': self.description, 'keywords': self.keywords,
+            'date': self.date
+            }
