4f1b5c48dc489e53f4014cd30897c5e131bbc5ca
boa <boa@protonmail.com>
2023-10-20 15:19:56 +0200

    add keyword generation process

--- webapp/auto_kmdb/article_processor.py
+++ webapp/auto_kmdb/article_processor.py
@@ -6,9 +6,22 @@ import requests
 from auto_kmdb.Article import Article
 from auto_kmdb.db import db
 
-LLM_SERVER = 'http://127.0.0.1:8085'
+CLASSIFICATION_SERVER = 'http://127.0.0.1:8085'
+KEYWORD_GENERATION_SERVER = 'http://127.0.0.1:8086'
 article_classification_prompt = '''{title}
 {description}'''
+keyword_generation_prompt = '''[címkék generálása]
+{title}
+
+{description}
+
+{text}
+
+cimkék: {keywords}
+
+###
+
+korrupciós címkék:'''
 
 
 def article_processor(appcontext):
@@ -16,17 +29,43 @@ def article_processor(appcontext):
     while True:
         query = Article.query.filter_by(is_classified=False)
         while query.count() > 0:
-            process(query.first())
-            query.first().is_classified = True
+            article = query.first()
+            process(article, do_classification)
+            db.session.commit()
+            if article.is_classified_corruption:
+                process(article, do_keyword_generation)
             db.session.commit()
         time.sleep(1*60)
 
 
-def process(article: Article):
+def do_classification(article: Article):
+    text = article_classification_prompt.format(title=article.title,
+            description=article.description, text=article.text,
+            keywords=', '.join(article.keywords))
+    resp = requests.post(CLASSIFICATION_SERVER+'/classify',
+                                 json={"text": text}, timeout=1800)
+    respText = resp.json()['class']
+    print(respText)
+    if respText == 'corruption':
+        print('found corruption', article.title)
+        article.is_classified_corruption = True
+    article.is_classified = True
+
+
+def do_keyword_generation(article: Article):
+    text = keyword_generation_prompt.format(title=article.title,
+            description=article.description, text=article.text,
+            keywords=', '.join(article.keywords))
+
+    resp = requests.post(KEYWORD_GENERATION_SERVER+'/completion',
+                                 json={"prompt": text}, timeout=1800)
+    respText = resp.json()['content'].strip()
+    print(respText)
+    article.tags = respText
+
+
+def process(article: Article, f):
     print('processing url:', article.url)
-    text = article_classification_prompt.format(
-        title=article.title, description=article.description, text=article.text, keywords=', '.join(article.keywords))
-    print(text)
     retry = 0
     while True:
         retry += 1
@@ -35,15 +74,8 @@ def process(article: Article):
             break
         try:
             t = time.time()
-            print('sending request to local bert server')
-            resp = requests.post(LLM_SERVER+'/classify',
-                                 json={"text": text}, timeout=1800)
-            respText = resp.json()['class']
-            print(respText)
-            if respText == 'corruption':
-                print('found corruption', article.title)
-                db.session.commit()
-                article.is_classified_corruption = True
+            print('sending request to local server')
+            f(article)
             elapsed_time = time.time() - t
             print('done in:', elapsed_time)
             break
