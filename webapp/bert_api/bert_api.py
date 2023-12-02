afe1514b6812e476218ff749c701ad66924f29ee
boa <boa@protonmail.com>
2023-12-02 22:09:05 +0100

    fix docker containers' launch command

--- webapp/bert_api/bert_api.py
+++ webapp/bert_api/bert_api.py
@@ -10,6 +10,10 @@ tokenizer = BertTokenizer.from_pretrained('SZTAKI-HLT/hubert-base-cc')
 app = Flask(__name__)
 
 
+@app.route("/status", methods=["GET"])
+def status():
+    return jsonify("OK"), 200
+
 @app.route("/classify", methods=["POST"])
 def index():
     data = request.get_json()
@@ -18,8 +22,4 @@ def index():
     probabilities = F.softmax(logits[0], dim=-1)
     res = torch.argmax(logits, axis=1)[0]
     corruption = probabilities[0] < 0.42
-    return jsonify({"class": "corruption" if corruption else "other", "prob": probabilities[0]}), 200
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8085)
+    return jsonify({"class": "corruption" if corruption else "other", "prob": float(probabilities[0])}), 200
